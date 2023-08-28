6f1616ba672b54b96c35c8fb66d2ad5873d14c8e
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-08-28 13:17:05 -0500

    Update routing

---
+++ my-app/components/navbar.tsx
@@ -0,0 +1,10 @@
+
+
+// <main className={styles.main}>
+//     <nav>
+//         <li>
+//         <ul><a href="about">About</a></ul>
+//         <ul><a href="blog">Blog</a></ul>
+//         </li>
+//         </nav>
+//     </main>
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import styles from './page.module.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
