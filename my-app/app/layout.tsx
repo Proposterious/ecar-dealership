c454a03f2335dc428c42353af10d0c343a10356a
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-08-29 11:02:23 -0500

    Update app routing

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from '../components/navbar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -17,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+        <header>
+          <Navbar />
+        </header>
     </html>
   )
 }
