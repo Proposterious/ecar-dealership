9ef4ef2213f879cf0ace755ccce23c7fd56469ab
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-09-05 10:46:16 -0500

    Replace 'prisma schema' => 'mongoose' for database

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <header>
           <Navbar />
-        </header>
+        </header>  
         {children}
         <footer>
           <Information />
