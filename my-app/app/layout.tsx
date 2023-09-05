e46664efaa958b5bdf62e9a1e9d8b3f066af7ebc
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-09-05 09:57:04 -0500

    Create visible shortcut icon for website.

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="shortcut icon" type="image/x-icon" href="../public/ecar-logo.ico" />
+      </head>
       <body className={inter.className}>
         <header>
           <Navbar />
