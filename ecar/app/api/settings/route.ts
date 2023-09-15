01fd65d2ea68d895acc0692f955b575c236525ed
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-09-14 20:48:15 -0500

    ISSUE: Incomplete settings route.ts

--- ecar/app/api/settings/route.ts
+++ ecar/app/api/settings/route.ts
@@ -1,36 +1,20 @@
-import bcrypt from 'bcrypt';
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
-
+// MAKE THIS FUNCTION UPDATE DATABASE WITH ENTERED INFORMATION
 export async function POST(req: Request) {
     const data = await req.json()
-    const name = data.name; const email = data.email; const password = data.password;
-    console.log(data)
-
-    if (!name || !email || !password) {
-        return new NextResponse('Missing name, email, or password...', { status: 400 })
-    }
-
-    const isValue: any = await prisma.user.findUnique({
-        where: { email: email },
+    const dataId = data.id as string;
+    const user: any = await prisma.user.findUnique({
+        where: { id: dataId },
     });
-    console.log({ isValue })
-    if (isValue != null) {
-        return new NextResponse("User already exists", { status: 405 })
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            hashedPassword
-        }
-    });
+    const 
+    // fullName: "",
+    //     phoneNumber: "",
+    //     name: "",
+    //     email: "",
+    //     bio: "",
     prisma.$disconnect
-    return NextResponse.json(user);
+    return NextResponse.json('NONE YET');
 }
