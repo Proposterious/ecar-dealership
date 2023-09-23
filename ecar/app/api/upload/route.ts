cb4beb7e083ab3803d25107cd1011fc199430626
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-09-23 17:47:51 -0500

    Despecify client vs server actions

--- ecar/app/api/upload/route.ts
+++ ecar/app/api/upload/route.ts
@@ -1,7 +1,7 @@
 // Specify as experimental feature
-'use server'
 export const config = { runtime: 'experimental-edge' }
 import { PrismaClient } from '@prisma/client' // connect to database
+import pako from 'pako'
 
 import { join } from 'path'
 import { writeFile } from 'fs/promises' // write new file with builtin function
@@ -19,16 +19,18 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({ success: false })
     }
     // Initialize instances
-    const prisma = new PrismaClient();
+    const prisma = new PrismaClient()
     const bytes = await file.arrayBuffer()
     const buffer = Buffer.from(bytes)
 
     // With the file data in the buffer, you can do whatever you want with it.
     // For this, we'll just write it to the filesystem in a new location
-    const path = join('/', 'tmp', file.name)
-    await writeFile(path, buffer)
-    console.log(`open ${path} to see the uploaded file`)
-    console.log(file)
-
+    prisma.$disconnect();
+    const path = join('/', 'from_host', file.name);
+    await writeFile(path, buffer);
+    console.log(`open ${path} to see the uploaded file`);
+    console.log(`File object\n`, file);
+    console.log(`Bytes \n`, bytes);
+    console.log(`Buffer of bytes \n`, buffer);
     return NextResponse.json({ success: true })
 }
