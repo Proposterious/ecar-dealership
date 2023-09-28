604cca70d14c24fdf754f8129c4cd099940f95bd
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-09-27 23:56:59 -0500

    Create custom navbar 'media' styles

--- ecar/tailwind.config.ts
+++ ecar/tailwind.config.ts
@@ -8,13 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+      // global
+        'xs': '0px', 
+      // navbar
+        'n-xs': '0px',
+        'n-sm': '738px',
+        'n-md': '800px',
+        'n-lg': '1025px',
+        'n-xl': '1174px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      scale: {
+        '-100': '-1',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }: any) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+  },
+  ],
 }
 export default config
