c9291d70462fe0bcaafc731d042b03484dec8c0c
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-02 01:00:08 -0500

    Update small navbar styles

--- ecar/components/content/header/_NavButton.tsx
+++ ecar/components/content/header/_NavButton.tsx
@@ -17,6 +17,7 @@ function NavButton() {
         if (icon.classList.contains(menu.open)) { // checks if already open
             icon?.classList.remove(menu.open) // if open, close
             container?.classList.remove('min-h-screen'); // remove styles
+            container?.classList.remove('w-screen'); // remove styles
             container?.classList.remove('bg-orange-600'); // remove
             block?.classList.remove('bg-slate-100'); // remove
             content?.classList.add('hidden') // hide list
@@ -26,6 +27,7 @@ function NavButton() {
         else {
             icon?.classList.add(menu.open)  // if closed, open
             container?.classList.add('min-h-screen'); // add styles
+            container?.classList.add('w-screen'); // add styles
             container?.classList.add('bg-orange-600'); // add
             block?.classList.add('bg-slate-100'); // add
             content?.classList.remove('hidden') // show list
@@ -34,39 +36,38 @@ function NavButton() {
     }
 
     return (
-        <article className='fixed float-left z-10 overscroll-none | n-md:hidden  pt-1'>
+        <article className='fixed float-left z-10 overscroll-none | n-md:hidden'>
             <div id='container'>
-                <div id='block' className='flex pb-1 border-b-4 border-r-4 border-white'>
-                    <div id={menu.navIcon} className='m-6' onClick={() => animate()}>
+                <div id='block' className='flex min-w-fit p-4 pb-6 border-b-4 border-r-4 border-white'>
+                    <div id={menu.navIcon} onClick={() => animate()}>
                         <span></span>
                         <span></span>
                         <span></span>
                         <span></span>
                     </div>
-                    <ul className ='list-none py-2'></ul>
                 </div>
 
-                <div id='content' className='hidden resize | flex flex-col flex-shrink gap-y-10 | p-8 mt-16 text-left | text-2xl text-white font-semibold cursor-pointer'>
-                    <span className='font-bold text-white hover:text-black'>
+                <div id='content' className='hidden resize min-h-screen h-full | flex flex-col items-center justify-evenly pb-12 | text-xl text-white font-semibold cursor-pointer'>
+                    <div className='font-bold text-white hover:text-black'>
                         <Link href='/' className="border-2 border-black rounded-2xl -ml-4 px-6 py-3 bg-orange-600">
                             Home
                         </Link>
-                    </span>
-                    <span className="hover:text-black">
+                    </div>
+                    <div className="hover:text-black">
                         <Link href='/about'>About</Link>
-                    </span>
-                    <span className="hover:text-black">
+                    </div>
+                    <div className="hover:text-black">
                         <Link href='/service'>Service</Link>
-                    </span>   
-                    <span className="hover:text-black">
+                    </div>   
+                    <div className="hover:text-black">
                         <Link href='/employment'>Employment</Link>
-                    </span>
-                    <span className="font-semibold hover:text-black ">
+                    </div>
+                    <div className="font-semibold hover:text-black">
                         <Link href='/support'>Support</Link>
-                    </span>
-                    <span className="font-semibold hover:text-black">
+                    </div>
+                    <div className="font-semibold hover:text-black">
                     <Link href='/dashboard'>Dashboard</Link>
-                    </span>
+                    </div>
                 </div> 
     
             </div>
