abce212ffe769fe08323ec5efae7d18c397a8ecb
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-05 12:53:04 -0500

    Test performance

--- ecar/app/auth/login/page.tsx
+++ ecar/app/auth/login/page.tsx
@@ -6,15 +6,14 @@ import LoginForm from './form';
 
 
 async function Login() {
-
-    const session = await getServerSession()
+    const session = await getServerSession();
     //redirect user to dashboard if logged in
     if (session != null) { 
         redirect('/dashboard') 
     } 
     
     return ( 
-        <div className='w-full h-full bg-orange-300'>
+        <div id="loginPage" className='w-full h-full bg-orange-300'>
             <LoginForm />
         </div>
     );
