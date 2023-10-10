85f05c06acb8b8fc8a59db8b0debc440beb4784f
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-10 16:41:12 -0500

    Test forEach(), map() on response.json

--- ecar/app/service/getCars.tsx
+++ ecar/app/service/getCars.tsx
@@ -2,7 +2,9 @@
 import { NextResponse } from 'next/server';
 
 export async function getCars() {
-    const url = 'https://car-api2.p.rapidapi.com/api/models?sort=id&direction=asc&year=2020&verbose=yes';
+
+    const url = 'https://car-api2.p.rapidapi.com/api/trims?limit=50&direction=asc&sort=id&year=2020&verbose=yes';
+
     const options = {
         method: 'GET',
         headers: {
@@ -12,16 +14,26 @@ export async function getCars() {
     };
 
     try {
+        // await fetch, assign variable
         const response = await fetch(url, options);
         const responseJSON = await response.json();
 
-        const carData = responseJSON.data;
+        const carData = [...responseJSON.data];
         console.log(carData)
-
         NextResponse.json({ success: 200 })
-        return carData;
+
+        console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
+
+        var carDict = {} as any;
+        carData.forEach(car => {
+            carDict[car.id] = car;
+            }
+        )
+
+        return carData; // return function
+
     } catch (error) {
-        console.error(error);
-        return NextResponse.json({ failure: error })
+        console.error(error)
+        return NextResponse.json({ failure: error }); // quit function
     }
 }
