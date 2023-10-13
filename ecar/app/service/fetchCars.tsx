f8a4ca8a3741f6461913b2c0d3367899b95c9d16
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-13 13:57:19 -0500

    Rename to 'getCars' to 'fetchCars'

--- ecar/app/service/fetchCars.tsx
+++ ecar/app/service/fetchCars.tsx
@@ -37,3 +37,31 @@ export async function getCars() {
         return NextResponse.json({ failure: error }); // quit function
     }
 }
+
+export async function getImages(array: []) {
+    
+
+    try {
+        // await fetch, assign variable
+        const response = await fetch(url, options);
+        const responseJSON = await response.json();
+
+        const carData = [...responseJSON.data];
+        console.log(carData)
+        NextResponse.json({ success: 200 })
+
+        console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
+
+        var carDict = {} as any;
+        carData.forEach(car => {
+            carDict[car.id] = car;
+            }
+        )
+
+        return carData; // return function
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ failure: error }); // quit function
+    }
+}
