89cd3a43252faf27fae3ca681b8ac7269c68c85f
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-13 13:57:43 -0500

    Update service page styles

--- ecar/app/service/page.tsx
+++ ecar/app/service/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import { getCars } from "./getCars";
+import { getCars, getImages } from "./fetchCars";
 
 function Service() {
     const [data, setData] = useState(null) as any;
@@ -15,38 +15,52 @@ function Service() {
 
     }
 
+    async function displayImage() {
+        
+    }
+
     return (
         <main className="min-h-screen w-full bg-slate-100 py-4 px-12">
-            <section className="max-w-screen mt-12 grid grid-flow-row grid-cols-4">
-                {data && data.map(car =>
-                    <ul key={car.id} className='w-fit font-semibold text-lg space-y-1 mb-12'>
 
-                        <li key="name" className='text-black font-bold'>
-                            Name: {car.make_model.make.name + ' ' + car.make_model.name}
-                        </li>
-                        
-                        <li key="id">
-                            Car #{car.id}
-                        </li>
+            <section>
+                <div className="max-w-screen mt-12 grid grid-flow-row grid-cols-4">
+                    {data && data.map(car =>
+                        <ul key={car.id} className='w-fit font-semibold text-lg space-y-1 mb-12'>
 
-                        <li key="type">
-                            <span className="type peer">Type: {car.name}</span>
-                            <span className="type tooltip peer-[.type]:hover:invisible |rounded shadow-lg p-1 bg-orange-600 text-slate-100">
+                            <li key="name" className='text-black font-bold'>
+                                Name: {car.make_model.make.name + ' ' + car.make_model.name}
+                            </li>
+                            
+                            <li key="id">
+                                Car #{car.id}
+                            </li>
+
+                            <li key="type">
+                                Type: {car.name}
+                            </li>
+
+                            <li key="make">
                                 Make: {car.description}
-                            </span>
-                        </li>
-                        
-                        
+                            </li>
+                            
+                            
 
 
-                    </ul>
-                    )
-                }
+                        </ul>
+                        )
+                    }
+                </div>
+
+                <button onClick={displayPage}>
+                    Click Here
+                </button>
             </section>
- 
-            <button onClick={displayPage}>
-                Click Here
-            </button>
+
+            <section>
+                <button onClick={displayPage}>
+                    Image testing
+                </button>
+            </section>
         </main>
     )
    
