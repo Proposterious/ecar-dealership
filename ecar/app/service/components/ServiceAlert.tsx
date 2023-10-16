ed56ad805689865df174d5c151ae4c2dfea50702
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-15 22:52:48 -0500

    Adjust Alert to be less awkward on small screens

--- ecar/app/service/components/ServiceAlert.tsx
+++ ecar/app/service/components/ServiceAlert.tsx
@@ -8,7 +8,7 @@ function ServiceAlert() {
 
     return ( 
     <section id="service-alert" className="w-full h-fit py-8 fixed bottom-0 bg-orange-600">
-        <div className="flex flex-row justify-evenly text-lg">
+        <div className="flex flex-col n-md:flex-row justify-evenly text-lg">
             <div className='font-semibold text-white text-center'>
                 This is an alert that pops up every time you enter this page. The content on this page will not appear unless you are signed in. We recommend you view this on a desktop or a larger screen.
             </div>
