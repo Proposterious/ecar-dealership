e9e654521b8ec15a83ab7e293bf71daed45f07c6
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-17 18:11:26 -0500

    Begin function getCarById()

--- ecar/app/service/handleCars.tsx
+++ ecar/app/service/handleCars.tsx
@@ -24,12 +24,6 @@ export async function getCars() {
         NextResponse.json({ success: 200 })
 
         console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
-
-        var carDict = {} as any;
-        carData.forEach(car => {
-            carDict[car.id] = car;
-            }
-        )
         
         NextResponse.json({ success: 200 })
         return carData; // return function
@@ -40,6 +34,36 @@ export async function getCars() {
     }
 }
 
-export async function getImages(cars: []) {
+export async function getCarById(id: string) {
+    
+    const url = 'https://car-api2.p.rapidapi.com/api/trims?limit=50&direction=asc&sort=id&year=2020&verbose=yes';
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '907f0a7383msh38d61721f0ac188p1b95e2jsn2a029f962ac0',
+            'X-RapidAPI-Host': 'car-api2.p.rapidapi.com'
+        }
+    };
+
+    try {
+        // await fetch, assign variable
+        const response = await fetch(url, options);
+        const responseJSON = await response.json();
+        console.log("Fetched data")
+
+        const carData = [...responseJSON.data];
+        console.log(carData)
+        NextResponse.json({ success: 200 })
+
+        console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
+        
+        NextResponse.json({ success: 200 })
+        return carData; // return function
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ failure: error }); // quit function
+    }
     
 }
