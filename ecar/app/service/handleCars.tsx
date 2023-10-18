30c156ff46f456991b905644fb81cb0b304dddbe
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-17 23:57:11 -0500

    Improve handleCars members

--- ecar/app/service/handleCars.tsx
+++ ecar/app/service/handleCars.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function getCars() {
 
-    const url = 'https://car-api2.p.rapidapi.com/api/trims?limit=50&direction=asc&sort=id&year=2020&verbose=yes';
+    const url = 'https://car-api2.p.rapidapi.com/api/trims?limit=36&direction=asc&sort=id&year=2020&verbose=yes';
 
     const options = {
         method: 'GET',
@@ -21,8 +21,6 @@ export async function getCars() {
 
         const carData = [...responseJSON.data];
         console.log(carData)
-        NextResponse.json({ success: 200 })
-
         console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
         
         NextResponse.json({ success: 200 })
@@ -35,8 +33,7 @@ export async function getCars() {
 }
 
 export async function getCarById(id: string) {
-    
-    const url = 'https://car-api2.p.rapidapi.com/api/trims?limit=50&direction=asc&sort=id&year=2020&verbose=yes';
+    const url = `https://car-api2.p.rapidapi.com/api/trims?direction=asc&sort=id&year=2020&make_model_id=${id}&verbose=yes`;
 
     const options = {
         method: 'GET',
@@ -52,11 +49,15 @@ export async function getCarById(id: string) {
         const responseJSON = await response.json();
         console.log("Fetched data")
 
-        const carData = [...responseJSON.data];
-        console.log(carData)
-        NextResponse.json({ success: 200 })
+        const carData = responseJSON.data;
 
-        console.log("\n\nCar make_model_make\n", carData[0].make_model.make)
+        if (carData === undefined) { 
+            console.log(carData);
+            NextResponse.json({ failure: 404, message: 'no id provided' }); ; // quit function
+            return;
+        }
+
+        console.log("car exists")
         
         NextResponse.json({ success: 200 })
         return carData; // return function
