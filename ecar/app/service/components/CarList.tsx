8b884f067ffc483d2a2c19c7258f797e9423d2fe
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-21 03:10:39 -0500

    Fix cars display;Experiment compact, expanded view

--- ecar/app/service/components/CarList.tsx
+++ ecar/app/service/components/CarList.tsx
@@ -1,103 +1,212 @@
 "use client"
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
+// built-in/dependencies shenanigans
+import Image from "next/image";
+import { useState, useEffect, useCallback } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
+// custom shenanigans
 import { getCars } from "../handleCars";
-
 import logo from "@/public/car-logo.png"
 import suvImage from "@/public/img/car-models/sketch_suv.jpg";
 import sedanImage from "@/public/img/car-models/sketch_sedan.jpg";
+import Loader from "@/app/loading";
 
-function CarList() {
-    const [data, setData] = useState(null) as any;
-    const [bool, setBool] = useState(null) as any;
 
-    async function displayImage() {
-        // Fetch data from 'carapi'
+function CarList() { 
+    // initiating nextjs vars
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()!
+
+    // initiating dynamic vars
+    const [ data, setData ] = useState([]);
+    const [ format, setFormat ] = useState("compact")
+
+    // fetch car data
+    async function formCars() {
+        // Fetch cars from 'carapi'
         var array = await getCars() as any;
-        var checkCars = [] as any; // define empty array
 
-
-        console.log('Should be the data as an array', array); // log prior to 'data' mutations
+        console.log('Should be the cars as an array', array); // log prior to 'cars' mutations
+        
+        // define new dictionary
+        var carData = {} as any;
 
         for (let i = 0; i < array.length; i++) {
-            // define str, strList from Car's description
+            // filter car by its name(s)
+            let carName = `${array[i].make_model.make.name} ${array[i].make_model.name}`;
+            console.log(carName)
+
+        // define str, strList from Car's description
             let str = array[i].description;
             let strList = str.split(' ');
 
-            if (checkCars.includes(array[i].name)) { // if already exists
-                console.log(`${array[i].name} already exists`) 
-            } else if (strList.includes("Sedan")) {
+        // handles images
+            if (strList.includes("Sedan")) {
                 array[i]["img"] = sedanImage;
                 console.log("was img atr", array[i].img)
                 console.log("has sedan", str); 
-                // Check if car in array
-                array[i]
+
             } else if (strList.includes("SUV")) {
                 array[i]["img"] = suvImage;
                 console.log("was img atr", array[i].img)
                 console.log("has suv", str);
+
             } else { 
                 array[i]["img"] = logo; 
                 console.log("type undefined or base")
             }
+
+
+            // Add car cars to the carData object
+            if (!carData[carName]) {
+                carData[carName] = [];
+            }
+            carData[carName].push(array[i]);
         }
+        console.log("carData", carData)
 
-        setData(array);
-        console.log("new array", array);
-        console.log("data after change", data);
-
-        setBool(true);
-
-        return;
+        var carDicts = Object.values(carData)
+        console.log("carDicts", carDicts)
+        
+        return carDicts
     }
 
-    // display cars onload
-    useEffect(() => {
-        displayImage();
-    }, []);
-  
-    return (
-            <section id="sell-cars" className='pt-2 pb-4 bg-orange-500'>
-            {/* Cars-List/Grid */}
-                <div id="car-list" className="min-h-screen w-full max-w-screen  bg-orange-500 | grid grid-flow-row n-xs:grid-cols-1 n-md:grid-cols-2 n-lg:grid-cols-4 px-12">  
-                    {bool && data.map(car =>
-                        
-                        <ul key={car.id} className='bg-slate-100 | font-semibold text-center text-lg space-y-1 | shadow-xs transition duration-300 ease-out hover:shadow-lg shadow-orange-700/70 rounded-lg | m-4 px-2 pt-6 pb-4 hover:cursor-default'>  
-                                                    
-                            <li key="img" className="w-fit mx-auto">
-                                <Image src={car.img} alt={car.name}
-                                className="bg-inherit"
-                                style={{objectFit:"contain", maxHeight:"238px"}} />
-                            </li>
-
-                            <li key="name" className='text-black font-bold'>
-                                Name: {car.make_model.make.name + ' ' + car.make_model.name}
-                            </li>
-                            
-                            <li key="id">
-                                Car #{car.make_model.id}
-                            </li>
-
-                            <li key="type" className="text-sm">
-                                Type: {car.name}
-                            </li>
-
-                            <li key="make" className="text-sm">
-                                Make: {car.description}
-                            </li>
-                            <li key="learn-more" className="pt-3">
-                                <button className="bg-orange-500 rounded-lg p-3 hover:text-white">
-                                        Learn More
-                                </button>
-                            </li>
-                        </ul>
-                        
-                    )}
-                </div>
-            </section>
+    // create new url params
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams)
+        params.set(name, value)
+    
+        return params.toString()
+    },
+    [searchParams]
     )
-   
+
+    function toggleFormat() {
+        // a) compact carList
+        if (format === "expand") { 
+            setFormat("compact");
+            router.push(pathname + '?' + createQueryString('format', 'compact'));
+            
+         }
+        // b) expand carList
+        else { 
+            setFormat("expand"); 
+            router.push(pathname + '?' + createQueryString('format', 'expand'));
+        }
+
+    }
+
+    useEffect(() => {
+        formCars().then((car: any) => setData(car));
+        console.log(data);
+    }, []);
+
+    return (
+    <>
+        <section className="flex flex-row | first:invisible hover:first:visible | fixed right-8 top-36">
+            <div id="carList-tooltip" className="bg-slate-200/90 px-3 py-2 m-3 | font-semibold text-sm text-slate-600">
+                <span id="explanation" className="border-b-2 border-orange-600">
+                    Click "?" to toggle between "Compact" and "Expand" formats
+                </span>
+
+                <br/>
+
+                <p id="format-desc" className="text-normal inline-block">
+                    <span className="underline decoration-orange-600 decoration-wavy | font-semibold">Compact</span>: view no duplicates 
+                    
+                    <br/>
+
+                    <span className="underline decoration-orange-600 decoration-wavy | font-semibold">Expand</span>: view duplicates of different types
+                </p>
+                
+            </div>
+            <span className="max-w-fit my-auto">
+                <button id="toggle-carFormat" className="bg-black/40 border-white border-2 ring-1 ring-orange-600 rounded-full px-4 py-1.5 | font-extrabold text-lg text-orange-600" onClick={toggleFormat}
+
+                onMouseEnter={() => {
+                    document.getElementById('carList-tooltip')!.classList.remove('invisible');
+                }}
+                onMouseLeave={() => {
+                document.getElementById('carList-tooltip')!.classList.add('invisible');
+                }}>
+                        ?
+                </button>
+            </span>
+        </section>
+        
+        {/* Display Loader while Fetching Data */}
+        {data.length < 2 && <Loader />}
+
+        {/* Display Cars after Fetching Data */}
+        <section className="bg-inherit | grid grid-cols-4 grid-flow-row">
+          {data && data.map((dict: any) => (
+             <div key={dict[0].id} className="space-y-4">
+            {/* display expanded form of carList */}
+              {format == "expand" && dict.map((car: any) => (
+                <ul key={car.id} className="bg-slate-100 font-semibold text-center text-lg space-y-1 shadow-xs transition duration-300 ease-out hover:shadow-lg shadow-orange-700/70 rounded-lg hover:cursor-default">
+                    <li key="img" className="w-fit mx-auto">
+                      <Image src={car.img} alt={car.name} className="bg-inherit" style={{ objectFit: "contain", maxHeight: "238px" }} />
+                    </li>
+    
+                    <li key="name" className="text-black font-bold">
+                      Name: {car.make_model.make.name + ' ' + car.make_model.name}
+                    </li>
+    
+                    <li key="id">
+                      Car #{car.make_model.id}
+                    </li>
+    
+                    <li key="type" className="text-sm">
+                      Type: {car.name}
+                    </li>
+    
+                    <li key="make" className="text-sm">
+                      Make: {car.description}
+                    </li>
+                    <li key="learn-more" className="pt-3">
+                      <button className="bg-orange-500 rounded-lg p-3 hover:text-white">
+                        Learn More
+                      </button>
+                    </li>
+                </ul>
+              ))}
+
+            {/* display compacted form of carList */}
+              {format == "compact" && (
+                <ul key={dict[0].id} className="bg-slate-100 font-semibold text-center text-lg space-y-1 shadow-xs transition duration-300 ease-out hover:shadow-lg shadow-orange-700/70 rounded-lg m-4 p-4 pb-4 hover:cursor-default">
+                    <li key="img" className="w-fit mx-auto">
+                    <Image src={dict[0].img} alt={dict[0].name} className="bg-inherit" style={{ objectFit: "contain", maxHeight: "238px" }} />
+                    </li>
+
+                    <li key="name" className="text-black font-bold">
+                    Name: {dict[0].make_model.make.name + ' ' + dict[0].make_model.name}
+                    </li>
+
+                    <li key="id">
+                    Car #{dict[0].make_model.id}
+                    </li>
+
+                    <li key="type" className="text-sm">
+                    Type: {dict[0].name}
+                    </li>
+
+                    <li key="make" className="text-sm">
+                    Make: {dict[0].description}
+                    </li>
+                    <li key="learn-more" className="pt-3">
+                    <button className="bg-orange-500 rounded-lg p-3 hover:text-white">
+                        Learn More
+                    </button>
+                    </li>
+                </ul>
+              )}
+            </div>
+          ))}
+        </section>
+    </>
+    );
 }
 
 export default CarList;
