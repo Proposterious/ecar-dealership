467e49931882db46d7bd55eae0a05204a696f501
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-21 03:11:38 -0500

    Fix DOM attribute exceptions

--- ecar/app/service/components/smallArrowDownWhite.tsx
+++ ecar/app/service/components/smallArrowDownWhite.tsx
@@ -1,7 +1,7 @@
 export default function smallArrowDownWhite() { 
     return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6 center">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        <path strokeLinecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
     </svg>
     )
 }
