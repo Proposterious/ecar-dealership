1548f54fb2a9fa8bbe978e3075385a019e47791e
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-24 22:48:59 -0500

    WIP: Create saved-vehicles page

---
+++ ecar/app/dashboard/saved-vehicles/page.tsx
@@ -0,0 +1,14 @@
+import DashboardHeader from "../components/DashboardHeader";
+
+
+function SavedVehicles() {
+    return (
+        <section id="saved-vehicles">
+            <div>
+                hi
+            </div>
+        </section>
+     );
+}
+
+export default SavedVehicles;
