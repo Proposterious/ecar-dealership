f338d527b3eb87dedc2846ca086d89d5689cb57a
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-27 14:56:02 -0500

    Patch 'update/route.ts' API response 3/3

--- ecar/app/api/update/route.ts
+++ ecar/app/api/update/route.ts
@@ -52,5 +52,5 @@ export async function POST(req: NextRequestWithAuth) {
     
     console.log('Completed User Update')
     await prisma.$disconnect();
-    return NextResponse.json({ message: 'Completed action' });
+    return;
 }
