ee865cd5ef13899ca6af5760b6ecb7156c1f965f
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-27 16:09:35 -0500

    Update types to match

--- ecar/types.d.ts
+++ ecar/types.d.ts
@@ -1,1 +1,14 @@
+import "next-auth";
 declare module 'bcrypt'; declare module 'prisma';
+
+declare module "next-auth" {
+    interface User {
+        fullName: string | null
+        phoneNumber: string | null
+    }
+
+    interface Session extends DefaultSession {
+        user?: User;
+    }
+}
+
