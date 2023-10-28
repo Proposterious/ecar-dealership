6bb9e6d23f7e3c7d9d632d99fbddbbf057651b7a
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-27 20:50:49 -0500

    Disable uploadImage for production

--- ecar/app/api/upload/route.ts
+++ ecar/app/api/upload/route.ts
@@ -1,43 +1,43 @@
-// Specify as experimental feature
-import { PrismaClient } from '@prisma/client' // connect to database
+// // Specify as experimental feature
+// import { PrismaClient } from '@prisma/client' // connect to database
 
-import { getToken } from 'next-auth/jwt' // get user's session/token
-import { NextResponse } from 'next/server' // JSON response with builtin function
-import { NextRequestWithAuth } from 'next-auth/middleware'
+// import { getToken } from 'next-auth/jwt' // get user's session/token
+// import { NextResponse } from 'next/server' // JSON response with builtin function
+// import { NextRequestWithAuth } from 'next-auth/middleware'
 
-// MAKE THIS FUNCTION UPDATE DATABASE WITH ENTERED INFORMATION
-const secret = process.env.NEXTAUTH_SECRET;
+// // MAKE THIS FUNCTION UPDATE DATABASE WITH ENTERED INFORMATION
+// const secret = process.env.NEXTAUTH_SECRET;
 
-export async function POST(req: NextRequestWithAuth) { 
-    console.log('running post')
-    // Get file from request
-    const file = await req.json() as unknown as string;
-    // Initialize instances
-    const prisma = new PrismaClient();
-    const session = await getToken({ req, secret }); // returns dictionary
-    console.log(session)
-    const checkId = session?.sub as string; // assigns id from token.id ('sub' object)
+// export async function POST(req: NextRequestWithAuth) { 
+//     console.log('running post')
+//     // Get file from request
+//     const file = await req.json() as unknown as string;
+//     // Initialize instances
+//     const prisma = new PrismaClient();
+//     const session = await getToken({ req, secret }); // returns dictionary
+//     console.log(session)
+//     const checkId = session?.sub as string; // assigns id from token.id ('sub' object)
     
-    // Search for user in database
-    const user = await prisma.user.findUnique({
-        where: {
-            id: checkId,
-        }
-    });
-    // Quit the function if user not found
-    if (!user) { 
-        console.log('Session Failed');
-        await prisma.$disconnect(); return NextResponse.json({ success: false, message: 'Failed'});
-    }
-    // Update user once verified
-    console.log('THIS ACCORDINGLY IS THE FILE SENT BY POST\n',file)
-    await prisma.user.update({
-        where: { id: checkId },
-        data: {
-            image: file,
-        },
-    })
+//     // Search for user in database
+//     const user = await prisma.user.findUnique({
+//         where: {
+//             id: checkId,
+//         }
+//     });
+//     // Quit the function if user not found
+//     if (!user) { 
+//         console.log('Session Failed');
+//         await prisma.$disconnect(); return NextResponse.json({ success: false, message: 'Failed'});
+//     }
+//     // Update user once verified
+//     console.log('THIS ACCORDINGLY IS THE FILE SENT BY POST\n',file)
+//     await prisma.user.update({
+//         where: { id: checkId },
+//         data: {
+//             image: file,
+//         },
+//     })
     
-    await prisma.$disconnect(); // close prisma's connection
-    return NextResponse.json({ success: true }) // return 'res.ok(200)'
-}
+//     await prisma.$disconnect(); // close prisma's connection
+//     return NextResponse.json({ success: true }) // return 'res.ok(200)'
+// }
