ae9d327b79ad604838b61ab7d737641a93c3a321
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-29 14:53:29 -0500

    Minor style changes in favor of small screens

--- ecar/components/content/footer/information.tsx
+++ ecar/components/content/footer/information.tsx
@@ -18,7 +18,7 @@ function Information() {
     return (  
         /* Profile as */
         <nav className="border-t-8 border-amber-600 bottom-full w-full divide-y-4 divide-gray-400/60">
-            <ul id='footer_social' className="list-none flex flex-wrap justify-center items-center space-x-16 p-3 h-30 bg-slate-100 text-orange-400">
+            <ul id='footer_social_md' className="list-none n-xs:hidden n-md:flex flex-wrap justify-center items-center space-x-16 p-3 h-30 bg-slate-100 text-orange-400">
                 <li>
                     <a target="_blank" rel="noopener noreferrer" href={yt_link}>
                         <Image src={Youtube}
@@ -48,7 +48,37 @@ function Information() {
                     </a>
                 </li>
             </ul>
-            <ul id='footer_notices' className="h-14 list-none flex flex-wrap justify-center items-center space-x-10 bg-gray-300">
+            <ul id='footer_social_xs' className="list-none n-md:hidden n-xs:flex flex-wrap justify-center items-center space-x-8 p-3 h-30 bg-slate-100 text-orange-400">
+                <li>
+                    <a target="_blank" rel="noopener noreferrer" href={yt_link}>
+                        <Image src={Youtube}
+                        height={70}
+                        width={100}
+                        alt='Youtube Profile' />
+                    </a>
+                </li>
+                <li className='ml-6'>
+                    <a target="_blank" rel="noopener noreferrer" href={github_link}>
+                        <Image src={GithubCircle} 
+                        height={70}
+                        width={40}
+                        alt='Github Profile' />
+                        <Image src={GithubWritten}
+                        height={70}
+                        width={40}
+                        alt='Github Profile' />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noopener noreferrer" href={li_link}>
+                        <Image src={LinkedIn}
+                        height={70}
+                        width={100}
+                        alt='LinkedIn Profile' />
+                    </a>
+                </li>
+            </ul>
+            <ul id='footer_notices' className="h-fit p-4 list-none flex flex-wrap justify-center items-center n-xs:space-x-4 n-xs:space-y-4 n-sm:space-y-0 n-sm:space-x-6 n-md:space-x-10 bg-gray-300">
                 <li className="inline-flex text-orange-600">
                     <a href='/'>Terms of Use</a>
                     <a href='/'><Image src={Popout} alt='' /></a> 
