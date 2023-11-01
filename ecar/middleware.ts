0f5ac3005d5ac4f3c24cd814b8d4786ad9284464
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-11-01 16:22:24 -0500

    Allow callbacks with any url

--- ecar/middleware.ts
+++ ecar/middleware.ts
@@ -1,10 +1,17 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
+export default withAuth(
+    function middleware(req) { // recognize req
+        console.log(req)
+  }, 
+  {
   pages: {
     signIn: "/auth/login",
     signOut: "/auth/logout",
     error: "/auth/error"
+  },
+  callbacks: {
+    authorized: ({ token }) => token?.role === "authorized",
   },
 });
 export const config = { matcher: ["/dashboard","/dashboard/:path*"] };
