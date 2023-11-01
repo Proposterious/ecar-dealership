d8ab42768e474a0c88913791e3a19194bd99f611
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-11-01 14:01:12 -0500

    chore:

--- ecar/app/dashboard/components/DashboardHeader.tsx
+++ ecar/app/dashboard/components/DashboardHeader.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 ;
 function DashboardHeader() {
+    var placeholderName;
     const path = usePathname();
     var pathname;
     console.log("path", path)
@@ -17,37 +18,10 @@ function DashboardHeader() {
 
     // Get userData as 'session' from useSession()
     const { data: session, status } = useSession({ required: true });
-    if (status == "loading") { // placeholderName 'Your' waiting for authentication
-        <div className="mb-6 flex flex-col gap-4">
-            <h2 className="text-2xl font-extrabold text-orange-600 underline">
-            Your Dashboard | { pathname }
-            </h2>
-
-            <nav>
-            <ol className="flex items-center gap-2">
-                <li>
-                    <Link href="/dashboard" className="font-semibold text-orange-600 duration-200 hover:font-bold hover:text-orange-500">
-                        Dashboard
-                    </Link>
-                </li>
-                <li>
-                    <p className="-indent-1 font-semibold">
-                        /
-                    </p>
-                </li>
-                <li>
-                    <Link href="path" className="-indent-1 font-semibold text-slate-700 duration-200 hover:font-bold hover:text-black">
-                        { pathname }
-                    </Link>
-                </li>
-            </ol>
-            </nav>
-
-        </div> 
-    }
+    if (status == "loading") { placeholderName = "Your" }
 
     // Assign placeholders variables with 'session' values
-    const placeholderName = session?.user?.name as string; // required for register/login
+    placeholderName = session?.user?.name as string; // required for register/login
 
     return ( 
         <div className="mb-6 flex flex-col gap-4">
