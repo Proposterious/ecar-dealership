1fa1f88423058f960b7ee7255dc5005c9d2f61d2
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-11-01 13:59:55 -0500

    Edit Loader styles

--- ecar/app/loading.tsx
+++ ecar/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
     return (
-        <main id="loader" className="flex justify-center items-center min-h-screen bg-slate-100 w-full">
+        <main id="loader" className="flex justify-center items-center min-h-full bg-slate-200 w-full">
             <div className='loader'></div>
         </main>
     )
