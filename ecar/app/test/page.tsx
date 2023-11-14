1c78ae0a98f239cad3eca560fea7a1de4d9c638a
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-11-14 08:53:00 -0600

    Attempt to create global cars `.json` file

---
+++ ecar/app/test/page.tsx
@@ -0,0 +1,13 @@
+function Test() {
+    return ( 
+        <>
+        <button onClick={() => {
+            console.log(process.cwd() + '/app/carInfo.json')
+        }}>
+            Check pathc
+        </button>
+        </>
+     );
+}
+
+export default Test;
